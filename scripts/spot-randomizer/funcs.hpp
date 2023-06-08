80d2983decdab076336828238066c766f6e43329
b-mayr-1984 <b.mayr.1984@googlemail.com>
2023-06-08 23:05:34 +0200

    prepare mission for spot randomization
    Final goal is that Zeus does not need to place spots manually anymore.

---
+++ scripts/spot-randomizer/funcs.hpp
@@ -0,0 +1,8 @@
+class spot_randomizer {
+    tag = "spot_randomizer";
+    class functions {
+        file = "scripts\spot-randomizer\functions";
+        class randomizeSpots{postInit = 1;};
+        class placeTask{};
+    };
+};
